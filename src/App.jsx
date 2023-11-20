74ce7099eee9625fa674348b5b5f1ae0899397b7
alpturan-dev <abdurrahmanalpturan13@hotmail.com>
2023-11-20 18:04:53 +0300

    add data visualition to map

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,15 @@ import MapChart from "./components/Map"
 function App() {
   return (
     <>
-      <h1 className="w-100 font-bold text-4xl text-center text-blue-500">Clean Water and Sanitation</h1>
+      <div className="mx-auto px-8 py-6 w-full flex flex-row justify-around gap-10 bg-[#002146]">
+        <h1 className="w-100 font-extrabold text-3xl">Clean Water and Sanitation</h1>
+        <ul className="text-base flex flex-row justify-center items-center gap-10">
+          <li className="hover:opacity-90"><a href="/">Map</a></li>
+          <li className="hover:opacity-90"><a href="/">Information</a></li>
+          <li className="hover:opacity-90"><a href="/">Resources</a></li>
+        </ul>
+      </div>
+      <hr className="border-0 w-full h-2 bg-slate-300" />
       <MapChart />
     </>
   )
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -7,43 +7,71 @@ import geojson from '../assets/custom.geo.json'
 import waterAndSanitation from '../assets/water-and-sanitation.csv?url'
 
 export default function MapChart() {
-    const [data, setData] = useState([]);
+    const [waterAndSanitationData, setWaterAndSanitationData] = useState([]);
     const [content, setContent] = useState("");
 
     useEffect(() => {
         d3.csv(waterAndSanitation).then((data) => {
-            setData(data);
+            const groupedData = data.reduce((acc, obj) => {
+                const foundEntity = acc.find(item => item.Entity === obj.Entity);
+
+                if (foundEntity) {
+                    const entityDataIndex = acc.findIndex(item => item.Entity === obj.Entity);
+                    acc[entityDataIndex].data.push({
+                        ...obj // Include all properties from the object
+                    });
+                } else {
+                    acc.push({
+                        Entity: obj.Entity,
+                        data: [{
+                            ...obj // Include all properties from the object
+                        }]
+                    });
+                }
+
+                return acc;
+            }, []);
+            console.log("groupedData", groupedData);
+            setWaterAndSanitationData(groupedData);
         });
     }, [])
+
     return (
         <div>
             <div data-tooltip-id="my-tooltip">
-                <ComposableMap projectionConfig={{
+                <ComposableMap width={900} height={window.innerHeight - 200} projectionConfig={{
                     rotate: [-10, 0, 0],
                     scale: 147
                 }}>
-                    <Sphere stroke="#E4E5E6" strokeWidth={0.5} />
-                    <Graticule stroke="#E4E5E6" strokeWidth={0.5} />
+                    <Sphere stroke="#888" strokeWidth={0.2} />
+                    <Graticule stroke="#aaa" strokeWidth={0.3} />
                     <Geographies geography={geojson} >
                         {({ geographies }) =>
                             geographies.map((geo) => {
+                                const item = waterAndSanitationData.filter((item) => item.Entity === geo.properties.name)[0];
                                 return <Geography
                                     key={geo.rsmKey}
                                     geography={geo}
                                     className="my-anchor-element"
                                     onMouseEnter={() => {
-                                        setContent(`${geo.properties.name}`);
+                                        console.log(item?.data[0]["Access to improved drinking water"])
+                                        setContent(
+                                            {
+                                                name: geo.properties.name,
+                                                data: item?.data[0]["Access to improved drinking water"]
+                                            }
+                                        );
                                     }}
                                     onMouseLeave={() => {
                                         setContent("");
                                     }}
                                     style={{
                                         default: {
-                                            fill: "#D6D6DA",
+                                            fill: item?.data[0] ? "#CBD5E1" : "#eee",
                                             outline: "none"
                                         },
                                         hover: {
-                                            fill: "lightblue",
+                                            fill: "#002146",
                                             outline: "none"
                                         },
                                         pressed: {
@@ -56,7 +84,14 @@ export default function MapChart() {
                     </Geographies>
                 </ComposableMap>
             </div>
-            <Tooltip id="my-tooltip" anchorSelect=".my-anchor-element" place="top">{content}</Tooltip>
-        </div>
+            <Tooltip id="my-tooltip" anchorSelect=".my-anchor-element" place="top">
+                <div className="text-lg">
+                    {content.name}
+                </div>
+                <div>
+                    {content.data ? `Access to improved drinking water: ${content.data}` : "No Data"}
+                </div>
+            </Tooltip>
+        </div >
     )
 }
